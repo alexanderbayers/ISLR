3e49220bea95dd8d44b15536e92073d16d76008e
Alex Bayers <alexander.bayers@gmail.com>
2018-09-19 21:41:54 +0100

    Added Ch2 Exercises
    Added ch 2

---
+++ ISLR Chapter 2.R
@@ -0,0 +1,8 @@
+#Problem 2.1
+#a. Flexible model - there is minimal variance because on average because there are so many samples
+#b. Less flexible model - Variance is extremely high so it's more likely to have spurious errors
+#c. Flexible model - we don't want to add too much bias that will prevent us from capturing this nonlinearity.
+#d. Lesss flexible model - high variance of error terms makes it more prone to overfitting.
+
+#Problem 2.2
+#a.  
